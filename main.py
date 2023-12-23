cec16e2e113efead369913d9d156656ac8449beb
caglenoah <caglenoah@gmail.com>
2023-12-23 08:32:05 -0700

    dict for dictionary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!" 
@@ -12,8 +13,12 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
     
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
+        
+    
 item1 = Item("Phone", 100, 5)
 item2 = Item("Laptop", 1000, 3)
            
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())           
+print(Item.__dict__) # All attributes for Class level  
+print(item1.__dict__) # All the attributes for instance level
